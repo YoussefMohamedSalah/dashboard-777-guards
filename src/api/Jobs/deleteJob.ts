6c5bc680fcf2371d2d068ad32cfb57437939d8c4
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2024-03-27 08:08:50 +0200

    .

--- src/api/Jobs/deleteJob.ts
+++ src/api/Jobs/deleteJob.ts
@@ -4,7 +4,7 @@ import { http } from "utils/Http";
 
 export const useDeleteJob = () => {
   return useMutation<any, Error, string>(async (id) => {
-    const { data } = await http.delete(ROUTES.JOBS + id + "/");
+    const { data } = await http.delete(ROUTES.JOBS + id);
     return { data: { data: data as any } };
   });
 };
